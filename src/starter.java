28b591a8096d353fdf0fd4a7f6542238021ec4ba
UnivDonguk22 <kampo5096@hanyang.ac.kr>
2022-12-09 02:28:44 +0900

    :art: edit starter.java Class
    스타터 클래스 수정

--- src/starter.java
+++ src/starter.java
@@ -2,7 +2,7 @@ import gui.start.StartGUI;
 
 import java.io.IOException;
 
-public class Starter {
+public class starter {
     public static void main(String[] args) throws IOException {
         new StartGUI();
     }
